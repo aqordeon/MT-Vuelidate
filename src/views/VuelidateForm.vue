6af188cc0a04e99372f017fd438a996cecfc2475
AqordeoN <aqordivika@yahoo.co.id>
2022-12-15 11:18:36 +0700

    menambahkan script GTM

--- src/views/VuelidateForm.vue
+++ src/views/VuelidateForm.vue
@@ -78,10 +78,10 @@
 				</div>
 			</div>
 		</form>
-		<!-- <t-table :headers="['Name', 'Email', 'Age', 'Sales']" :data="[
+		<t-table :headers="['Name', 'Email', 'Age', 'Sales']" :data="[
 			['Alfonso Bribiesca', 'alfonso@vexilo.com', '31', '$9,999.00'],
 			['Saida Redondo', 'saida@gmail.com', 27, '$124.00'],
-		]"></t-table> -->
+		]"></t-table>
 
 	</div>
 
